ce5c92f3f45bae946584e325382edfe48b51c305
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2020-12-14 23:00:02 -0300

    OrderStatus enum / Category

--- src/main/java/com/iftm/course/entities/Order.java
+++ src/main/java/com/iftm/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.iftm.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -25,6 +26,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -32,10 +35,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -62,6 +66,16 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 	@Override
 	public int hashCode() {
@@ -87,7 +101,5 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
